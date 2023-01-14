f7fb32dff0a303288652222652a11487859b9d05
TheReVeaLz <70447633+TheReVeaLz@users.noreply.github.com>
2023-01-15 02:30:17 +0700

    Create youtubeGain.js

---
+++ youtubeGain.js
@@ -0,0 +1,54 @@
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+var video = document.querySelector('video');
+try {
+    var audioCtx = new AudioContext();
+    var source = audioCtx.createMediaElementSource(video);
+    var gainNode = audioCtx.createGain();
+    source.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    gainState = 0;
+} catch {
+    executionState = 0;
+}
+
+if (!(!!document.getElementById('gainInput'))) {
+    var video = document.querySelector('video');
+    var vidTitle = document.querySelector('.ytd-watch-metadata');
+    var gainVal = document.createElement('input');
+    gainVal.id = "gainInput";
+    gainVal.style = "width:25%;height:18px;font-size:18px;border-radius: 15px;padding:0px 8px;";
+    gainVal.type = "text";
+    gainVal.placeholder = "Gain Volume";
+    gainVal.value = "1";
+    gainVal.classList.add('yt-spec-button-shape-next--tonal', 'yt-spec-button-shape-next--mono');
+    gainVal.onclick = function() {
+        this.select()
+    };
+	function limitCheck(i) {
+        if (i > 5) {
+            return 5;
+        } else if (i < 1) {
+            return 1;
+        }
+		return i;
+	}
+    gainVal.onmousewheel = function(e) {
+        e.preventDefault();
+        if (e.deltaY < 0) {
+            this.value++;
+        } else {
+            this.value--;
+        }
+		this.value=limitCheck(this.value);
+		gainNode.gain.value = gainVal.value;
+    }
+    gainVal.onchange = function() {
+		this.blur();
+		gainVal.value=limitCheck(this.value);
+		gainNode.gain.value = gainVal.value;
+    }
+    vidTitle.insertBefore(gainVal, vidTitle.children[0]);
+}
